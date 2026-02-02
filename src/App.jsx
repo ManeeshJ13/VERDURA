8866ca8afd5de53362ab4b9573a4f4f69823c546
Maneesh James <maneeshjamesa@gmail.com>
2026-02-02 20:53:11 +0530

    fixed mainpage issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
 import LoadingScreen from "./loadingScreen";
-import MainPage from './MainPage';
+import MainPage from "./mainPage";
 
 const App = () => {
   return(
